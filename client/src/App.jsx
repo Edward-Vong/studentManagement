a1a38b7940efe0526712b54af08fa0da4c6804c0
maikeruzu <54655520+maikeruzu@users.noreply.github.com>
2024-04-08 18:13:55 -0700

    mockup pages with no actual styling oops
    Admin page uses dummy data to just sort of simulate what the admin page looks like when it views classes? (it should be relatively the same as the student view when looking for classes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import SignUpPage from "./components/SignUpPage";
+import LoginPage from "./components/LoginPage";
+import LandingPage from "./components/LandingPage";
+import AdminPage from "./components/AdminPage";
 
 const App = () => {
   return (
     <div className="w-full p-6">
-      <Navbar />
-      <Outlet />
+      <AdminPage />
     </div>
   );
 };
