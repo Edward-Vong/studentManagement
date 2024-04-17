570baba9415bbc451d328bb2955986a5c213c161
PandaIsAwkward <64350799+Edward-Vong@users.noreply.github.com>
2024-04-17 01:33:42 -0700

    Revert "Switch to mysql"

---
+++ README.md
@@ -0,0 +1,10 @@
+Need to add config.env posted on discord.
+
+Starting server connection:
+1. cd server 
+2. node --env-file=config.env server
+
+Start web page host:
+* In new terminal
+1. cd client
+2. npm run dev
--- server/server.js
+++ server/server.js
@@ -1,32 +1,15 @@
-require('dotenv').config({path: 'config.env'});
+import express from "express";
+import cors from "cors";
+import records from "./routes/record.js";
 
-const express = require('express');
-const mysql = require('mysql2');
-const bodyParser = require('body-parser');
-
+const PORT = process.env.PORT || 5050;
 const app = express();
-const port = 3000;
 
-app.use(bodyParser.json());
+app.use(cors());
+app.use(express.json());
+app.use("/record", records);
 
-// Set up MySQL connection
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-});
-
-connection.connect(error => {
-  if (error) throw error;
-
-  else console.log("Successfully connected to the database.");
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
