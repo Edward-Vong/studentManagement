b99feb203d169e3a58c103ec073503b8a688e7a5
Michael <maikeruzu@users.noreply.github.com>
2024-04-23 10:33:48 -0700

    landingpage update :3

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,24 +1,20 @@
 import { NavLink } from "react-router-dom";
+import "../styles/LandingPage.css"; 
 
-export default function Navbar() {
+export default function LandingPage() {
     return (
-
-        <div className="landingpage flex flex-col items-center justify-center h-screen">
-        
-            <div className="mb-8">
-                <img alt="EduControl_Logo" className="w-48" src="../assets/EduContol.png" />
+        <div className="landing-page-container">
+            <div className="logo-container">
+                <img alt="EduControl_Logo" className="logo" src="../assets/EduContol.png" />
             </div>
-
-            <div className="flex space-x-4">
-                <NavLink to="/login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <div className="button-container">
+                <NavLink to="/login" className="login-button">
                     Login
                 </NavLink>
-                <NavLink to="/signup" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <NavLink to="/signup" className="signup-button">
                     Sign Up
                 </NavLink>
             </div>
-
         </div>
-
     );
 }
