8cb03e59c559f06c12640df1e3eb41ea02c26dc9
Edward Vong <evusa142@gmail.com>
2024-04-25 20:05:30 -0700

    installed router-dom and set up routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
 import SignUpPage from "./components/SignUpPage";
 import LoginPage from "./components/LoginPage";
 import LandingPage from "./components/LandingPage";
@@ -7,15 +8,21 @@ import AdminPage from "./components/AdminPage";
 import StudentPage from './components/StudentPage';
 import InstructorPage from './components/InstructorPage';
 
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <InstructorPage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/student" element={<StudentPage />} />
+        <Route path="/instructor" element={<InstructorPage />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
-export default App
+}
 
+export default App;
