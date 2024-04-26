5848de0e52046947a1d262f2dc1f9a9d9b43cfbc
Edward Vong <evusa142@gmail.com>
2024-04-25 19:57:54 -0700

    corrected server configuration

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
-require('dotenv').config();
-
+require('dotenv').config({ path: 'config.env' });
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
