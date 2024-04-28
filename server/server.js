db83d8acbeae344cb5e89b66588aa2b5d33b7cff
Edward Vong <evusa142@gmail.com>
2024-04-27 22:04:24 -0700

    Proto endpoints: delete user/dpt/room, post (dpt, room), and modify user

--- server/server.js
+++ server/server.js
@@ -27,14 +27,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-
-
-
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
-
 
 // CREATE USER
 app.post('/signup', async (req, res) => {
@@ -86,3 +81,116 @@ app.post('/login', (req, res) => {
     }
   });
 });
+
+app.delete('/users/:id', async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+      const connection = await pool.getConnection();
+
+      const [result] = await connection.query('DELETE FROM users WHERE id = ?', [userId]);
+
+      connection.release();
+
+      if (result.affectedRows === 1) {
+          res.status(200).json({ message: 'User deleted successfully' });
+      } else {
+          res.status(404).json({ error: 'User not found' });
+      }
+  } catch (error) {
+      console.error('Error deleting user:', error);
+      res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.put('/users/:id', async (req, res) => {
+  const userId = req.params.id;
+  const userData = req.body; 
+
+  try {
+      const connection = await pool.getConnection();
+      const [result] = await connection.query('UPDATE users SET ? WHERE id = ?', [userData, userId]);
+
+      connection.release();
+
+      if (result.affectedRows === 1) {
+          res.status(200).json({ message: 'User updated successfully' });
+      } else {
+          res.status(404).json({ error: 'User not found' });
+      }
+  } catch (error) {
+      console.error('Error updating user:', error);
+      res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.post('/department', async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const { Name, CollegeName } = req.body;
+    const query = 'INSERT INTO departments (DepartmentName, CollegeName) VALUES (?, ?)';
+    const [results] = await connection.promise().execute(query, [Name, CollegeName]);
+
+    res.status(201).json({ message: 'Department created successfully', results: results });
+  } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: 'Error creating department', error: error });
+  }
+});
+
+app.delete('/department/:id', async (req, res) => {
+  try {
+    const connection = await pool.getConnection();
+
+    const [result] = await connection.query('DELETE FROM department WHERE id = ?', [DepartmentID]);
+
+    connection.release();
+
+    if (result.affectedRows === 1) {
+      res.status(200).json({ message: 'Department deleted successfully' });
+    } else {
+      res.status(404).json({ error: 'Department not found' });
+    }
+  } catch (error) {
+    console.error('Error deleting Department:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.post('/rooms', async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const { Building, RoomNum, Capacity } = req.body;
+    const query = 'INSERT INTO rooms (Building, RoomNum, Capacity) VALUES (?, ?, ?';
+    const [results] = await connection.promise().execute(query, [Building, RoomNum, Capacity]);
+    
+    res.status(201).json({ message: 'Room added successfully', results: results });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error adding room', error: error });
+  }
+})
+
+app.delete('/rooms/:id', async (req, res) => {
+  try {
+    const connection = await pool.getConnection();
+
+    const [result] = await connection.query('DELETE FROM Rooms WHERE id = ?', [RoomID]);
+
+    connection.release();
+
+    if (result.affectedRows === 1) {
+      res.status(200).json({ message: 'Room deleted successfully' });
+    } else {
+      res.status(404).json({ error: 'Room not found' });
+    }
+  } catch {
+    console.error('Error removing Room', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+})
+
+
+
